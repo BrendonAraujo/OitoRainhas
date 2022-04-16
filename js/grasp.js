2313bd23edb4b1ce69cb6bf871828c7052ab7ac6
BrendonAraujo <brendong.araujo@gmail.com>
2022-04-16 19:36:40 -0300

    Adcionado função para marcar todas as celulas em diagonal que estiverem em cheque

--- js/grasp.js
+++ js/grasp.js
@@ -3,26 +3,105 @@ function iniciar(){
     var posicao;
     var quant_rainhas = 1;
 
-    while (quant_rainhas < 9){
-        posicao = Math.floor(Math.random() * (64 - 1) + 1);
-        receba = document.getElementById(posicao);
-        if(!possuiuRainha(posicao)){
-            rainha = document.getElementById('r'+quant_rainhas);
-            receba.insertAdjacentElement("beforeend",rainha);
-            receba.setAttribute('value',1);
-            quant_rainhas = quant_rainhas + 1;
-        }
-        
-    } 
-    
+    posicao = 9//Math.floor(Math.random() * (64 - 1) + 1);
+    receba = document.getElementById(posicao);
+    rainha = document.getElementById('r'+quant_rainhas);
+    receba.insertAdjacentElement("beforeend",rainha);
+    receba.setAttribute('value',1);
+    receba.style.border = "5px solid #fc0303";
+    marcaPosicao(posicao);
 }
 
-function possuiuRainha(atualPosition){
-    var retorno = false;
-    var posicao = document.getElementById(atualPosition);
+function marcaPosicao(id){
 
-    if(posicao.childElementCount > 0){
-        retorno = true;
+    var coluna = document.getElementById(id).parentNode;
+    var diferencaDireita = 8 - coluna.getAttribute('value');
+    var celula;
+    var i = coluna.getAttribute("firstElement");
+    var alterador;
+
+    console.log(i);
+    console.log(coluna.getAttribute("lastElement"));
+    console.log(i >= coluna.getAttribute("lastElement"));
+    // marca ceculas verticais
+    while (i <= coluna.getAttribute("lastElement")){
+        celula = document.getElementById(i);
+        celula.style.border = "5px solid #fc0303";
+        celula.setAttribute('value',1);
+        i++;
     }
-    return retorno;
+
+
+    // controla ajustes superiores
+    if(id != coluna.getAttribute("firstElement")){
+        i = 1;
+        celula = 0;
+        alterador = '';
+
+        // marca células da direita superior da posicao
+        while (i <= diferencaDireita){
+            celula = (id + (8 * i)) - i;
+            alterador = document.getElementById(celula);
+            if(alterador.getAttribute("id") == alterador.parentNode.getAttribute("firstElement")){
+                i = diferencaDireita;
+            }
+            alterador.style.border = "5px solid #fc0303";
+            alterador.setAttribute('value',1);
+            i++;
+        }
+        
+        // limpa variáveis
+        i = 1;
+        celula = 0;
+        alterador = ''; 
+
+        // marca células da esquerda superior da posicao
+        while (i < coluna.getAttribute('value')){
+            celula = (id - (8 * i)) - i;
+            alterador = document.getElementById(celula);
+            if(alterador.getAttribute("id") == alterador.parentNode.getAttribute("firstElement")){
+                i = coluna.getAttribute('value');
+            }
+            alterador.style.border = "5px solid #fc0303";
+            alterador.setAttribute('value',1);
+            i++;
+        }
+    }
+
+    // controla ajustes inferiores
+    if(id != coluna.getAttribute("lastElement")){
+        // limpa variáveis
+        i = 1;
+        celula = 0;
+        alterador = ''; 
+
+        // marca células da direita inferior da posicao
+        while (i <= diferencaDireita){
+            celula = (id + (8 * i)) + i;
+            alterador = document.getElementById(celula);
+            if(alterador.getAttribute("id") == alterador.parentNode.getAttribute("lastElement")){
+                i = diferencaDireita;
+            }
+            alterador.style.border = "5px solid #fc0303";
+            alterador.setAttribute('value',1);
+            i++;
+        }
+
+        // limpa variáveis
+        i = 1;
+        celula = 0;
+        alterador = ''; 
+
+         // marca células da esqueda inferior da posicao
+        while (i < coluna.getAttribute('value')){
+            celula = (id - (8 * i)) + i;
+            alterador = document.getElementById(celula);
+            if(alterador.getAttribute("id") == alterador.parentNode.getAttribute("lastElement")){
+                i = coluna.getAttribute('value');
+            }
+            alterador.style.border = "5px solid #fc0303";
+            alterador.setAttribute('value',1);
+            i++;
+        }
+    }
 }
